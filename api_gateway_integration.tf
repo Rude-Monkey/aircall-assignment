cce2f309232aeaf224b3593d30dce60a821ce6a1
Vincent Gilles <vgilles@forepaas.com>
2020-05-10 15:50:14 +0200

    adding output value for the api path

--- api_gateway_integration.tf
+++ api_gateway_integration.tf
@@ -1,3 +1,7 @@
+output "api_url" {
+    value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/${aws_api_gateway_resource.image.path_part}"
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name = "resize_api"
 
--- bucket.tf
+++ bucket.tf
@@ -1,4 +1,7 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "aircall-test-resized-images"
   acl    = "public-read"
+
+  #Needed for a dev purpose. You might not want to add this option on a production bucket as it will allow it to be destroyed even when it is not empty.
+  force_destroy = true
 }
